69034c0a50908121291ac82dc523f9862ce8ff85
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-04-27 21:28:44 +0200

    develop builder

--- hdr/argument.hpp
+++ hdr/argument.hpp
@@ -1,3 +1,6 @@
+#ifndef _ARGUMENT_H_
+#define _ARGUMENT_H_
+
 #include <string>
 #include <vector>
 #include <optional>
@@ -12,6 +15,10 @@ public:
         return value.has_value() ? value : default_val;
     };
 
+    void set(const std::string& new_value) {
+        value = new_value;
+    }
+
     bool is_triggered(const std::string& option) {
         return option == name || (option.size() == 1 && option[0] == name[0]);
     }
@@ -20,4 +27,7 @@ protected:
     OptionValue value;
     OptionValue default_val;
     std::string name;
+    std::string description;
 };
+
+#endif // _ARGUMENT_H_
--- hdr/argumentBuilder.hpp
+++ hdr/argumentBuilder.hpp
@@ -1,3 +1,6 @@
+#ifndef _ARGUMENT_BUILDER_H_
+#define _ARGUMENT_BUILDER_H_
+
 #include "argument.hpp"
 
 #include <string>
@@ -12,6 +15,25 @@ public:
         arg->name = name;
     }
 
+    std::shared_ptr<Argument> get() 
+    {
+        return arg;
+    }
+
+    ArgumentBuilder& default_value(const std::string& def)
+    {
+        arg->default_val = def;
+        return *this;
+    }
+
+    ArgumentBuilder& with_description(const std::string& description)
+    {
+        arg->description = description;
+        return *this;
+    }
+
 private:
-    std::unique_ptr<Argument> arg;
+    std::shared_ptr<Argument> arg;
 };
+
+#endif // _ARGUMENT_BUILDER_H_
