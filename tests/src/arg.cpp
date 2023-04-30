1ea7eeec591fcd0ddda639c79f22db342d9c7cc9
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-04-30 11:56:22 +0200

    add uts

--- tests/src/arg.cpp
+++ tests/src/arg.cpp
@@ -1,12 +1,14 @@
 #include "gtest/gtest.h"
 #include "arg.hpp"
+#include "util.hpp"
+#include <format>
 
 using namespace rcp;
 
 constexpr char TEST_ARG_NAME[]{"test_name"};
 
 TEST(ArgTest, setDefaultValue_returnDefaultValue) {
-    std::string defval{"some_value"};
+    const std::string defval{"some_value"};
     auto arg = ArgBuilder(TEST_ARG_NAME).default_value(defval).get();
     ASSERT_EQ(defval, arg->get().value());
 }
@@ -14,4 +16,31 @@ TEST(ArgTest, setDefaultValue_returnDefaultValue) {
 TEST(ArgTest, noDefaultValue_returnNone) {
     auto arg = ArgBuilder(TEST_ARG_NAME).get();
     ASSERT_FALSE(arg->get().has_value());
+}
+
+TEST(ArgTest, valueSet_returnsValue) {
+    auto arg = ArgBuilder(TEST_ARG_NAME).get();
+    const std::string expected{"some_value"};
+    arg->set(expected);
+    ASSERT_EQ(expected, arg->get().value());
+}
+
+TEST(ArgTest, defaultValValueSet_returnsNewValue) {
+    auto arg = ArgBuilder(TEST_ARG_NAME).default_value("any").get();
+    const std::string expected{"some_value"};
+    arg->set(expected);
+    ASSERT_EQ(expected, arg->get().value());
+}
+
+TEST(ArgTest, descriptionSet_returnsHelpWithDescription) {
+    const std::string test_desc{"test description"};
+    auto arg = ArgBuilder(TEST_ARG_NAME).with_description(test_desc).get();
+    const std::string expected = std::format("-{}  --{}  <{}>  {}", TEST_ARG_NAME[0], TEST_ARG_NAME, util::upper(TEST_ARG_NAME), test_desc);
+    ASSERT_EQ(expected, arg->help());
+}
+
+TEST(ArgTest, isTriggered_triggeredByShortAndLongVersion) {
+    auto arg = ArgBuilder(TEST_ARG_NAME).get();
+    ASSERT_TRUE(arg->is_triggered(TEST_ARG_NAME));
+    ASSERT_TRUE(arg->is_triggered(std::string{TEST_ARG_NAME[0]}));
 }
