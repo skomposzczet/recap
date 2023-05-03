40c8bed7a28995b606dd301334a2aeccc5da3d8c
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-05-03 17:23:05 +0200

    add cat util

--- hdr/util.hpp
+++ hdr/util.hpp
@@ -2,12 +2,22 @@
 #define _UTIL_H_
 
 #include <string>
+#include <sstream>
 
 namespace rcp {
 
 namespace util {
 
 std::string upper(std::string str);
+
+template<typename... Args>
+std::string cat(Args&&... args)
+{
+    std::ostringstream oss;
+    (oss << ... << std::forward<Args>(args));
+    return oss.str();
+}
+
 
 }
 
