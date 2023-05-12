9854771c7b4952ff0696c66c5eed5841b58c8ebd
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-05-12 23:25:41 +0200

    add ok or method and templated err factory method to result

--- hdr/result.hpp
+++ hdr/result.hpp
@@ -28,6 +28,11 @@ public:
             throw RcpError("Tried to get result ok, when it is err.");
         return std::get<type>(_v);
     }
+    type get_ok_or(const type& or_val) {
+        if (is_ok())
+            return std::get<type>(_v);
+        return or_val;
+    }
     const err_type& get_err() const {
         if (is_ok())
             throw RcpError("Tried to get result err, when it is ok.");
@@ -53,6 +58,13 @@ public:
 
     static Result<> ok() {
         return Result<>{};
+    }
+
+    template<typename T>
+    static Result<T> err(const std::string& err_str) {
+        Result<T> res;
+        res._v = err_str;
+        return res;
     }
 
     static Result<> err(const std::string& err_str) {
